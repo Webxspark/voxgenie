e6f75dea3c9c0ea06d75dbe8e262a6108a8db4ef
Alan Christofer <webxspark@gmail.com>
2024-08-16 14:31:04 +0530

    Basic ssl for vite compiler removed (reason:cookies cross-origin error)

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,8 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import basicSsl from '@vitejs/plugin-basic-ssl'
 export default defineConfig({
-  plugins: [react(), basicSsl()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
