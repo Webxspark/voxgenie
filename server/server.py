99437b4dccdd9f6729fef494495f40de21dadf6b
Alan Christofer <webxspark@gmail.com>
2024-09-05 22:46:41 +0530

    update: -t streak

--- server/server.py
+++ server/server.py
@@ -442,7 +442,7 @@ def handle_disconnect():
 
 
 if(__name__ == "__main__"):
-  # app.run(debug=False)
+  # app.run(debug=False) # streak 
   os.system("clear")
   print(f"Server running on port 5000\nENDPOINT: http://localhost:5000")
   socketio.run(app, debug=False)
